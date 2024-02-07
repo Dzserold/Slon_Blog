fee2d40af7b844606295fb083df46c548abe02d4
Dzserold <f.dzserold@gmail.com>
2024-02-07 01:26:33 +0100

    adding favicon logo and setting

---
+++ src/lib/components/Nav.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import logo from "@/images/logo.svg";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="flex">
+        <Image src={logo} alt="Slon Blog Logo" height={40} width={40} />
+        <h1 className="">Slon Blog</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
