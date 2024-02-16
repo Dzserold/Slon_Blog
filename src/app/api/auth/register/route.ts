62578de4349a7c89a4ddda05126ea21d631c01e8
Dzserold <f.dzserold@gmail.com>
2024-02-16 16:42:09 +0100

    adding hashed with bcrypt

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,12 +1,20 @@
+import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
 
 export async function POST(request: Request) {
   try {
+    await sql`CREATE TABLE IF NOT EXISTS users( id SERIAL PRIMARY KEY,
+      id SERIAL PRIMARY KEY,
+      email VARCHAR(50) UNIQUE NOT NULL,
+      password VARCHAR(50) NOT NULL`;
+
     const { email, password } = await request.json();
     //validate email and password
-    console.log({ email, password });
-    return NextResponse.json({ message: "Succes" });
-  } catch (error) {
-    console.error({ error });
+    const hashedPassword = await hash(password, 10);
+    const response = await sql`INSERT INTO users (email, password)
+       VALUES (${email},${hashedPassword})`;
+  } catch (error: any) {
+    console.log(error);
   }
 }
