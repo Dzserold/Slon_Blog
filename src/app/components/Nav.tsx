830659feaf66319373748a72dd1620bb45ebfffc
Dzserold <f.dzserold@gmail.com>
2024-03-15 22:31:06 +0100

    adding nextauth v5 and boilerplate

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,32 +1,50 @@
 import Image from "next/image";
 import logo from "@/images/logo.svg";
 import Link from "next/link";
+import { auth } from "auth";
 
-const Nav = () => {
+const Nav = async () => {
+  const sesssion = await auth();
   return (
     <nav className="flex justify-between px-4 pt-1">
       <Link href="/">
         <div className="flex items-center p-1">
-          <Image src={logo} alt="Slon Blog Logo" height={50} width={50} />
+          <Image
+            src={logo}
+            alt="Slon Blog Logo"
+            height={50}
+            width={50}
+          />
           <h1 className="text-3xl font-bold text-dark_pink">
             Slon<span className="text-pink">Blog</span>
           </h1>
         </div>
       </Link>
       <div className="flex">
+        {sesssion && sesssion.user ? (
+          <div>{sesssion.user?.email}</div>
+        ) : null}
         <ul className="flex">
           <Link href="/">
-            <li className="p-3 text-pink hover:text-dark_pink">HOME</li>
+            <li className="p-3 text-pink hover:text-dark_pink">
+              HOME
+            </li>
           </Link>
           <Link href="/">
-            <li className="p-3 text-pink hover:text-dark_pink">HOME</li>
+            <li className="p-3 text-pink hover:text-dark_pink">
+              HOME
+            </li>
           </Link>
           <Link href="/">
-            <li className="p-3 text-pink hover:text-dark_pink">HOME</li>
+            <li className="p-3 text-pink hover:text-dark_pink">
+              HOME
+            </li>
           </Link>
         </ul>
         <Link href="/login">
-          <h3 className="p-2 text-lg font-bold hover:text-dark_pink">Login</h3>
+          <h3 className="p-2 text-lg font-bold hover:text-dark_pink">
+            Login
+          </h3>
         </Link>
       </div>
     </nav>
