a3c10a9e2b0b391498ab9c17cbf67e784ab399d7
Dzserold <f.dzserold@gmail.com>
2024-03-31 22:56:27 +0200

    updated create post and console log result off form

--- src/app/profile/[id]/newPost/page.tsx
+++ src/app/profile/[id]/newPost/page.tsx
@@ -3,45 +3,97 @@ import Image from "next/image";
 import plusImg from "@/images/plus.svg";
 import xImg from "@/images/x.svg";
 import { useState } from "react";
+import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface Input {
   name: string;
   value: string;
 }
 
+const FormSchema = z.object({
+  title: z
+    .string()
+    .min(2, "Title must be at least 2 characters")
+    .max(50, "Title must be less than 50 characters"),
+  content: z
+    .string()
+    .min(2, "Content should be at least 2 characters")
+    .max(500, "Content  must be less than 500 characters"),
+  categories: z.array(
+    z
+      .string()
+      .nonempty({ message: "Category name is required" })
+      .min(2, "Category name must be at least 2 characters")
+  ),
+});
+
+type InputType = z.infer<typeof FormSchema>;
+
 export default function Home() {
+  // Handle categories
   const [inputs, setInputs] = useState<Input[]>([]);
 
   const handleAddClick = () => {
-    setInputs([...inputs, { name: "", value: "" }]);
-  };
-
-  const handleChange = (
-    index: number,
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const newInputs = [...inputs];
-    newInputs[index].value = event.target.value;
-    setInputs(newInputs);
+    setInputs([
+      ...inputs,
+      { name: `${inputs.length}`, value: "" },
+    ]);
   };
 
   const handleRemoveClick = (index: number) => {
-    const newInputs = [...inputs];
-    newInputs.splice(index, 1);
-    setInputs(newInputs);
+    // Remove the item at the specified index
+
+    setInputs((prevState) =>
+      prevState.filter((item) => item.name !== index.toString())
+    );
+
+    // Unregister the removed input from React Hook Form
+    unregister(`categories.${index}`);
+
+    console.log(inputs);
   };
+
+  //Validate and handle Form publish
+  const {
+    control,
+    register,
+    handleSubmit,
+    unregister,
+    formState: { errors },
+  } = useForm<InputType>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const publishPost: SubmitHandler<InputType> = async (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl">Making new Post</h1>
-      <form className="flex flex-col w-8/12 gap-3 text-dark">
+      <form
+        onSubmit={handleSubmit(publishPost)}
+        className="flex flex-col w-8/12 gap-3 text-dark"
+      >
         <input
+          {...register("title")}
           placeholder="Title"
           className="px-3 py-1 border-4 border-black rounded-md outline-none text-md focus:border-dark_pink"
         />
+        {errors.title && (
+          <p className="mt-2 italic text-md text-error">
+            {errors.title?.message}
+          </p>
+        )}
 
         <button
+          disabled={inputs.length === 9}
           type="button"
-          className="flex items-center justify-center px-1 rounded-md bg-pink"
+          className={`${
+            inputs.length > 8 ? "bg-dark_pink" : "bg-pink"
+          } flex items-center self-center justify-center px-1 rounded-md w-60 `}
           onClick={handleAddClick}
         >
           ADD NEW CATEGORY
@@ -53,17 +105,21 @@ export default function Home() {
             width={30}
           />
         </button>
+        {errors.categories && (
+          <p className="mt-2 italic text-md text-error">
+            {errors.categories?.message}
+          </p>
+        )}
+
         <div className="grid grid-cols-2 gap-2 md:grid-cols-3">
           {inputs.slice(0, 9).map((input, index) => (
-            <div className="flex">
+            <div className="flex" key={index}>
               <input
                 key={index}
+                {...register(`categories.${index}`)}
                 placeholder="Category"
                 type="text"
                 className="w-full px-3 py-1 mr-2 border-4 border-black rounded-md outline-none text-md focus:border-dark_pink"
-                name={`input-${index}`}
-                value={input.value}
-                onChange={(event) => handleChange(index, event)}
               />
               <button
                 type="button"
@@ -82,11 +138,16 @@ export default function Home() {
         </div>
 
         <textarea
+          {...register("content")}
           rows={8}
           placeholder="Content"
           className="px-3 py-1 border-4 border-black rounded-md outline-none text-md focus:border-dark_pink"
         />
-
+        {errors.content && (
+          <p className="mt-2 italic text-md text-error">
+            {errors.content?.message}
+          </p>
+        )}
         <button
           type="submit"
           className="self-center inline-block text-lg font-bold rounded-md w-36 bg-pink hover:bg-dark_pink"
