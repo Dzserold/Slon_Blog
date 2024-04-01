06193e28f07cab7b8713c75a155ecee87a6b7b79
Dzserold <f.dzserold@gmail.com>
2024-04-01 02:29:29 +0200

    going back to prev version for better ux

--- src/app/profile/[id]/newPost/page2.tsx
+++ src/app/profile/[id]/newPost/page2.tsx
@@ -1,16 +1,11 @@
 "use client";
 import Image from "next/image";
 import plusImg from "@/images/plus.svg";
-import xImg from "@/images/x.svg";
 import { useEffect, useState } from "react";
 import { z } from "zod";
+
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-interface Input {
-  name: string;
-  value: string;
-}
 
 const FormSchema = z.object({
   title: z
@@ -33,25 +28,31 @@ type InputType = z.infer<typeof FormSchema>;
 
 export default function Home() {
   // Handle categories
-  const [inputs, setInputs] = useState<Input[]>([]);
-  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [categories, setCategories] = useState<string[]>([]);
 
-  const handleAddClick = () => {
-    setInputs([...inputs, { name: `${counter}`, value: "" }]);
-    setCounter(counter + 1);
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputValue(event.target.value);
   };
 
-  const handleRemoveClick = (index: number) => {
+  const addCategories = () => {
+    if (inputValue === "" || inputValue === " ") {
+      return;
+    }
+    setCategories((prev) => [...prev, inputValue]);
+    setInputValue("");
+  };
+
+  const removeCategory = (index: number) => {
     // Remove the item at the specified index
-
-    setInputs((prevState) =>
-      prevState.filter((item) => item.name !== index.toString())
+    setCategories((prevArray) =>
+      prevArray
+        .slice(0, index)
+        .concat(prevArray.slice(index + 1))
     );
-
-    // Unregister the removed input from React Hook Form
     unregister(`categories.${index}`);
-
-    console.log(inputs);
   };
 
   //Validate and handle Form publish
@@ -67,10 +68,9 @@ export default function Home() {
   const publishPost: SubmitHandler<InputType> = async (data) => {
     console.log(data);
   };
-
   useEffect(() => {
     console.log(errors.categories);
-  }, [errors.categories]);
+  }, [errors?.categories]);
 
   return (
     <div className="flex flex-col items-center gap-4">
@@ -90,53 +90,56 @@ export default function Home() {
           </p>
         )}
 
-        <button
-          disabled={inputs.length === 9}
-          type="button"
-          className={`${
-            inputs.length > 8 ? "bg-dark_pink" : "bg-pink"
-          } flex items-center self-center justify-center px-1 rounded-md w-60 `}
-          onClick={handleAddClick}
-        >
-          ADD NEW CATEGORY
-          <Image
-            className="p-2"
-            src={plusImg}
-            alt="Plus icon"
-            height={30}
-            width={30}
-          />
-        </button>
         {errors.categories && (
           <p className="mt-2 italic text-md text-error">
             {errors.categories?.message}
           </p>
         )}
-
-        <div className="grid grid-cols-2 gap-2 md:grid-cols-3">
-          {inputs.slice(0, 9).map((input, index) => (
-            <div className="flex" key={input.name}>
-              <input
-                key={index}
-                {...register(`categories.${Number(input.name)}`)}
-                placeholder="Category"
-                type="text"
-                className="w-full px-3 py-1 mr-2 border-4 border-black rounded-md outline-none text-md focus:border-dark_pink"
-              />
-              <button
-                type="button"
-                onClick={() => handleRemoveClick(index)}
-              >
-                <Image
-                  className="w-6"
-                  src={xImg}
-                  alt="X icon"
-                  height={40}
-                  width={40}
+        <div className="grid grid-cols-2 text-center gap-y-3 md:grid-cols-3 min-h-8 text-light ">
+          {categories[0] ? (
+            categories.map((category, index) => (
+              <div className="p-0 py-2 " key={index}>
+                <input
+                  value={category}
+                  className="p-2 text-center rounded-md appearance-none cursor-default hover:border-r-dark_pink hover:bg-error caret-transparent focus:outline-none bg-dark_pink"
+                  contentEditable={false}
+                  {...register(`categories.${index}`)}
+                  onClick={() => removeCategory(index)}
                 />
-              </button>
-            </div>
-          ))}
+              </div>
+            ))
+          ) : (
+            <p className="col-span-3">
+              Add Category tags to your post
+            </p>
+          )}
+        </div>
+        <div className="flex">
+          <input
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Category"
+            type="text"
+            className="w-full px-3 py-1 mr-2 border-4 border-black rounded-md outline-none text-md focus:border-dark_pink"
+          />
+
+          <button
+            disabled={categories.length === 9}
+            type="button"
+            className={`${
+              categories.length > 8 ? "bg-dark_pink" : "bg-pink"
+            } flex items-center self-center justify-center px-1 rounded-md w-60 `}
+            onClick={addCategories}
+          >
+            ADD NEW
+            <Image
+              className="p-2"
+              src={plusImg}
+              alt="Plus icon"
+              height={30}
+              width={30}
+            />
+          </button>
         </div>
 
         <textarea
