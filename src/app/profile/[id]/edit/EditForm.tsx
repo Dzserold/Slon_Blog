15a77bb6800377180a617eb35bc1649a8bd04183
Dzserold <f.dzserold@gmail.com>
2024-04-03 22:49:40 +0200

    made edit page UI

---
+++ src/app/profile/[id]/edit/EditForm.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z
+  .object({
+    userName: z
+      .string()
+      .min(2, "First Name must be at least 2 characters")
+      .max(45, "First Name must be less than 45 characters")
+      .regex(
+        new RegExp("^[a-zA-Z0-9.@_-]+$"),
+        "This username is not valid"
+      ),
+    email: z
+      .string()
+      .email("Please enter a valid email address"),
+
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .max(50, "Password must be at less than 50 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password and confirm password doesn't match",
+    path: ["confirmPassword"],
+  });
+
+type InputType = z.infer<typeof FormSchema>;
+
+export default function EditForm({ id }: { id: string }) {
+  console.log(id);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputType>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const update: SubmitHandler<InputType> = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <h1 className="p-4 text-3xl font-bold">Register</h1>
+      <h3 className="mb-3 text-sm font-light">
+        If you would like to create an account, please register
+      </h3>
+      <form
+        onSubmit={handleSubmit(update)}
+        className="flex flex-col gap-3 text-dark"
+      >
+        <input
+          placeholder="Username"
+          className="px-3 py-1 border-4 border-black rounded-md outline-none w-72 text-md focus:border-dark_pink"
+          {...register("userName")}
+        />
+        {errors.userName && (
+          <p className="mt-2 italic text-md text-error">
+            {errors.userName?.message}
+          </p>
+        )}
+        <input
+          placeholder="Email"
+          className="px-3 py-1 border-4 border-black rounded-md outline-none w-72 text-md focus:border-dark_pink"
+          type="email"
+          {...register("email")}
+        />
+        {errors.email && (
+          <p className="mt-2 italic text-md text-error">
+            {errors.email?.message}
+          </p>
+        )}
+        <input
+          placeholder="Password"
+          className="px-3 py-1 border-4 border-black rounded-md outline-none w-72 text-md focus:border-dark_pink"
+          type="password"
+          {...register("password")}
+        />
+        {errors.password && (
+          <p className="mt-2 italic text-md text-error">
+            {errors.password?.message}
+          </p>
+        )}
+        <input
+          placeholder="Connfirm Password"
+          className="px-3 py-1 border-4 border-black rounded-md outline-none w-72 text-md focus:border-dark_pink"
+          type="password"
+          {...register("confirmPassword")}
+        />
+        {errors.confirmPassword && (
+          <p className="mt-2 italic text-md text-error">
+            {errors.confirmPassword?.message}
+          </p>
+        )}
+        <button
+          type="submit"
+          className="inline-block text-lg font-bold rounded-md bg-pink hover:bg-dark_pink"
+        >
+          REGISTER
+        </button>
+      </form>
+      <h3 className="mt-3 text-sm font-light">
+        Already have an account?
+        <Link
+          className="ml-1 text-base text-pink hover:text-dark_pink"
+          href="/login"
+        >
+          Login
+        </Link>
+      </h3>
+    </div>
+  );
+}
--- src/app/profile/[id]/edit/page.tsx
+++ src/app/profile/[id]/edit/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>edit</div>;
+"use sever";
+import EditForm from "./EditForm";
+
+export default function page({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return <EditForm id={params.id} />;
 }
